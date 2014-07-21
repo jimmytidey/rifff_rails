b32c646842ca62821c8c4213589b225dba746c41
Jimmy Tidey <jimmytidey@gmail.com>
2014-07-21 15:58:01 +0100

    removing dead key from code

--- config/initializers/s3_direct_upload.rb
+++ config/initializers/s3_direct_upload.rb
@@ -1,6 +1,6 @@
 S3DirectUpload.config do |c|
-  c.access_key_id = "AKIAIBKPLZ3UOTAJXILA"       # your access key id
-  c.secret_access_key = "IsyJSsj6Iy02OhsQYH37hqszNxyZUcwu1yTgG7sZ"   # your secret access key
+  c.access_key_id =  ENV["aws_access_key_id"]      # your access key id
+  c.secret_access_key = ENV["aws_secret_access_key"]   # your secret access key
   c.bucket = "rifff_bucket"              # your bucket name
   #c.region = ""              # region prefix of your bucket url (optional), eg. "s3-eu-west-1"
 end
